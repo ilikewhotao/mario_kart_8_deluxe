949aad77072a793e0202401ac890743588496ab1
ilikewhotao <ilikewhotao@qq.com>
2024-11-20 17:55:23 +0800

    first

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -1,37 +1,37 @@
-import { defineAsyncComponent } from 'vue'
+// import { defineAsyncComponent } from 'vue'
 
-// import HomeView from '../views/HomeView.vue'
-// import UserView from '../views/UserView.vue'
-// import RecordView from '../views/RecordView.vue'
-// import FindView from '../views/FindView.vue'
+import HomeView from '../views/HomeView.vue'
+import UserView from '../views/UserView.vue'
+import RecordView from '../views/RecordView.vue'
+import FindView from '../views/FindView.vue'
 
-const _import = (path: string) =>
-  defineAsyncComponent(() => import(`../views/${path}.vue`))
+// const _import = (path: string) =>
+//   defineAsyncComponent(() => import(`../views/${path}.vue`))
 
 const routes = [
   { path: '/', redirect: '/home' },
   {
     path: '/home',
     name: 'home',
-    component: _import('HomeView'),
+    component: HomeView,
     meta: { title: '规则一览', icon: '📜' }
   },
   {
     path: '/user',
     name: 'user',
-    component: _import('UserView'),
+    component: UserView,
     meta: { title: '参赛名单', icon: '🎟' }
   },
   {
     path: '/record',
     name: 'record',
-    component: _import('RecordView'),
+    component: RecordView,
     meta: { title: '全部记录', icon: '📃' }
   },
   {
     path: '/find',
     name: 'find',
-    component: _import('FindView'),
+    component: FindView,
     meta: { title: '记录查询', icon: '💯' }
   }
 ]
